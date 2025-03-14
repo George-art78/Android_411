142fc3d107bd1b4754264aed8fd21da5cca07c63
George Remizov <g.remizov78@gmail.com>
2025-03-14 16:01:27 +0300

    first commit

---
+++ app/src/main/java/com/example/first/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.first;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textSample;
+    private Button secondButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        EdgeToEdge.enable(this);
+//        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.first_layout);
+//        setContentView(R.layout.second_layout);
+//        setContentView(R.layout.linear_layout_2);
+//        setContentView(R.layout.table_layout);
+//        setContentView(R.layout.frame_layout);
+//        setContentView(R.layout.scroll_view);
+        setContentView(R.layout.third_layout);
+
+        textSample = findViewById(R.id.textSample);
+        secondButton = findViewById(R.id.secondButton);
+
+        secondButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textSample.setText("Новое приветствие");
+                textSample.setTextSize(40);
+            }
+        });
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+    }
+
+    public void sayHello(View view) {
+        textSample.setText("Привет");
+    }
+}
